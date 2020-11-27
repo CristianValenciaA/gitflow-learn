b929279496ed26c8a122c2ac3a4e0559621bcf5a
Cristian <cristian.valenciaa@alumnos.uv.cl>
2020-11-27 18:53:26 -0300

    terminando mi wereable

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Supongamos que puedes tocar la ropa y cambiar tu musica.
         </p>
         <a
           className="App-link"
